19a3ed58a88e6a0911ccd4eaddc5b665ee12b63c
Chad Freeman <chadfreeman@koverse.com>
2018-02-25 15:53:12 -0700

    cleaning up some warnings

--- src/main/scala/com/koverse/example/spark/WordCounter.scala
+++ src/main/scala/com/koverse/example/spark/WordCounter.scala
@@ -25,9 +25,9 @@ import org.apache.spark.sql.functions.lower
 class WordCounter(
     textFieldName: String,
     tokenizationString: String) extends java.io.Serializable {
-  
+
   def count(inputRecordsRdd: RDD[SimpleRecord]): RDD[SimpleRecord] = {
-    
+
     // for each Record, tokenize the specified text field and count each occurrence
     val wordCountRdd = inputRecordsRdd.flatMap { record => record.get(textFieldName).toString().split(tokenizationString) }
                            .map { token => token.toLowerCase().trim() }
@@ -43,17 +43,17 @@ class WordCounter(
       record.put("count", count)
       record
     }}
-    
+
     outputRdd
   }
-  
+
   def count(inputDataFrame: DataFrame): DataFrame = {
-    
+
      // Take the column that contains the text and tokenize and count the words
     val wordDF = inputDataFrame.explode(textFieldName, "word") { (text: String) => text.split(tokenizationString) }
     wordDF.select(lower(col("word")).as("lowerWord"))
           .groupBy("lowerWord")
           .count()
   }
-  
+
 }
--- src/test/scala/com/koverse/example/spark/WordCounterTest.scala
+++ src/test/scala/com/koverse/example/spark/WordCounterTest.scala
@@ -30,42 +30,41 @@ import com.holdenkarau.spark.testing.DataFrameSuiteBase
  */
 @RunWith(classOf[JUnitRunner])
 class WordCounterTest extends DataFrameSuiteBase{
-  
+
   test("RDD test") {
     val inputRecords = List(
         new SimpleRecord(Map[String,Object]("text" -> "these words are to be counted", "id" -> "0").asJava),
         new SimpleRecord(Map[String,Object]("text" -> "more words    that are worth counting", "id" -> "1").asJava))
-        
+
     val inputRecordsRdd = sc.parallelize(inputRecords)
     val wordCounter = new WordCounter("text", """['".?!,:;\s]+""")
     val outputRecordsRdd = wordCounter.count(inputRecordsRdd)
-    
 
     assertEquals(outputRecordsRdd.count, 10)
     val outputRecords = outputRecordsRdd.collect()
     val countRecordOption = outputRecords.find { simpleRecord => simpleRecord.get("word").equals("are") }
-    
+
     assertTrue(countRecordOption.isDefined)
     assertEquals(countRecordOption.get.get("count"), 2)
   }
-  
+
   case class Message(text: String, id: String)
 
   test("DataFrame test") {
-       
+
     val messages = List(
         Message("these words are to be counted", "0"),
         Message("more words that are worth counting", "1"))
-        
+
     val inputDataFrame = sqlContext.createDataFrame(messages)
     val wordCounter = new WordCounter("text", """['".?!,:;\s]""")
     val outputDataFrame = wordCounter.count(inputDataFrame)
-    
+
     assertEquals(outputDataFrame.count(), 10)
     val outputRows = outputDataFrame.collect()
     val countRowOption = outputRows.find { row => row.getAs[String]("lowerWord").equals("are") }
     assertTrue(countRowOption.isDefined)
     assertEquals(countRowOption.get.getAs[Long]("count"), 2)
-    
+
   }
 }
